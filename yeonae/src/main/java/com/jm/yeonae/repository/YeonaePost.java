dffb043611d3586f4350f2f9844229bb5e773d9d
MK <pb1123love@gmail.com>
2021-05-11 21:28:21 +0900

    YeonaePost 클래스 생성

---
+++ yeonae/src/main/java/com/jm/yeonae/repository/YeonaePost.java
@@ -0,0 +1,49 @@
+package com.jm.yeonae.repository;
+
+public class YeonaePost {
+    private String title;
+    private String content;
+    private String location;
+    private String author;
+    private String create_at;
+
+    public String getCreate_at() {
+        return create_at;
+    }
+
+    public void setCreate_at(String create_at) {
+        this.create_at = create_at;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
