1ebcd963ed1147f4c2545a404c04a318da32df8c
MK <pb1123love@gmail.com>
2021-05-11 20:57:38 +0900

    Spring boot init

---
+++ yeonae/src/main/java/com/jm/yeonae/YeonaeApplication.java
@@ -0,0 +1,13 @@
+package com.jm.yeonae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeonaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeonaeApplication.class, args);
+	}
+
+}
